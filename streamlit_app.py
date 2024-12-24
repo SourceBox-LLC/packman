a77deeef7cf5c3e03bced34fd10eb3811f786392
sbussiso <sbussiso321@gmail.com>
2024-12-24 08:36:10 -0800

    minor changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if 'show_delete_pack_selectbox' not in st.session_state:
 session = boto3.Session(
     aws_access_key_id=os.getenv('ACCESS_KEY'),
     aws_secret_access_key=os.getenv('SECRET_KEY'),
-    region_name=os.getenv('REGION')
+    region_name='us-east-1'
 )
 
 # Create a Lambda client
